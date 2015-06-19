13320c743997a6b8a223e6d6da6286ca93021eab
Jason Avery <javery@mpirik.com>
2015-06-19 11:47:29 -0500

    Using node-trello to make API calls, and passed key and token as environment variables

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 var app = require('express')();
 var bodyParser = require('body-parser');
+var Trello = require('node-trello');
 
-app.use(bodyParser.json());         
+var devKey = process.env.DEV_KEY;
+var appToken = process.env.APP_TOKEN;
+var trello = new Trell(devKey, appToken);
+
+app.use(bodyParser.json());
 
 app.post('/', function (req, res) {
   console.log(req.body);
