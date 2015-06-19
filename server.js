07494aa6315c98114de4474373b20dc75cff87b3
Joseph Betlej <jbetlej@mpirik.com>
2015-06-19 10:34:35 -0500

    Added the License, the .gitignore file, and initial server code.

---
+++ server.js
@@ -0,0 +1,11 @@
+var app = require('express')();
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json());         
+
+app.post('/', function (req, res) {
+  console.log(req.body);
+  res.send({ status: 'I have received the message.' })
+});
+
+var server = app.listen(8080);
